8ec57513a720f640a91f6e45fc1f0e07d6f78340
Swong87 <8swordz@gmail.com>
2017-07-20 18:29:23 -0400

    added my comments

--- clozeCard.js
+++ clozeCard.js
@@ -1,17 +1,22 @@
+//Constructor for the cloze cards
 function ClozeCard (text, cloze){
 	if(this instanceof ClozeCard) {
 		this.fullText = text;
 		this.cloze = cloze;
 		this.partial = function(){
+			//Checks if the cloze word appears in the input sentence
 			if(this.fullText.includes(this.cloze)){
+				//Replaces the cloze word with a blank in the sentence
 				return this.fullText.replace(this.cloze, "________");
 			} else {
+				//If the cloze word does not appear in the sentence, it will alert the user
 				var logText = "'" + this.cloze + 
 					"' doesn't appear in '" + this.fullText + "'";
 				return logText;
 			}
 		}
 	} else {
+		//Will create new ClozeCard even if "new" is not typed
 		return new ClozeCard(text, cloze);
 	}
 }
--- main.js
+++ main.js
@@ -2,11 +2,12 @@ var fs = require("fs");
 var inquirer = require("inquirer");
 // var BasicCard = require("./basicCard");
 var ClozeCard = require("./clozeCard");
-
+//This will hold the user created flash cards
 var cards = [];
 // var basicCards = [];
+//This will keep track of which flash card has been viewed when user views all
 var count = 0;
-
+//This starts the app giving the user options
 var startApp = function(){
 	inquirer
 	  	.prompt([
@@ -29,7 +30,7 @@ var startApp = function(){
 	  		}
 	  	});
 }
-
+//This will create a flash card with the answer on the back and the cloze deleted sentence on front
 var createCard = function(){
 	inquirer
 		.prompt([
@@ -45,10 +46,11 @@ var createCard = function(){
 	    }
 		])
 		.then(function(text) {
+			//Create a card object with user's input
 			var myCloze = ClozeCard(text.full, text.cloze);
-
+			//Push the created card to the array
 			cards.push(myCloze);
-
+			//Return to the previous options
 			startApp();
 		})
 }
@@ -76,31 +78,43 @@ var createCard = function(){
 // 		})
 // }
 
+//This will show a random flash card from the cards array
 var viewRandom = function(){
 	var randomNumber = Math.floor(Math.random() * cards.length);
+	//If there are no cards created it will inform the user
 	if(typeof cards[0] == "undefined"){
 		console.log("\nYou need to create a flash cards first!\n")
+		//Return to the previous options
 		startApp();
 	} else {
+		//shows the front of a random flash card
 		console.log("\n" + cards[randomNumber].partial() + "\n");
+		//This will ask the user if they want to see the answer
 		showAnswer(cards[randomNumber]);
 	}
 }
+//This function will go through the whole array of cards one by one
 var viewAll = function(){
 	if(typeof cards[0] == "undefined"){
+		//If there are no cards created it will inform the user
 		console.log("\nYou need to create a flash cards first!\n")
+		//Return to the previous options
 		startApp();
 	} else {
 		if(count < cards.length){
+			//Will show each card's front one by one 
 			console.log("\n" + cards[count].partial() + "\n");
+			//Will ask if the user wants to see the answer
 			showAnswerAll(cards[count]);
+			//Will make the app move to the next card
 			count++;
 		} else {
+			//Return to the previous options
 			startApp();
 		}
 	}
 }
-
+//Will allow the user to view each answer or get out of viewing all the cards
 var showAnswerAll = function(thisCard){
 	inquirer
 	  	.prompt([
@@ -120,7 +134,7 @@ var showAnswerAll = function(thisCard){
 			}
 		})
 }
-
+//Will show the answer for a flash card
 var showAnswer = function(thisCard){
 	inquirer
 	  	.prompt([
@@ -134,10 +148,13 @@ var showAnswer = function(thisCard){
 	  	.then(function(inquirerResponse) {
 	  		if(inquirerResponse.action === "Show Answer"){
 				console.log("\n" + thisCard.cloze + "\n");
+				//Return to the previous options
 				startApp();
 			} else {
+				//Return to the previous options
 				startApp();
 			}
 		})
 }
+//Start the app
 startApp();
